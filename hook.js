66969ab9d7d2bdee24a8c98ab2a1d8561d3f91bd
Tree Some <youn@tree-some.dev>
2020-06-05 01:09:46 +0900

    hook

--- hook.js
+++ hook.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 
 if ( fs.existsSync('dist/index.js') ) {
 	fs.readFile('dist/index.js', { encoding: 'utf8' }, (err, data) => {
+		console.log(data);
 		axios({
 			url: 'https://us-central1-kakaobot-63224.cloudfunctions.net/hook/build-complete',
 			method: 'put',
