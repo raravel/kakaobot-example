d5bd6e81046a6049cda02649c899dd3ba5271497
Tree Some <youn@tree-some.dev>
2020-06-05 01:20:23 +0900

    change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
     if ( msg.indexOf("!테스트") === 0 ) {
-        replier.reply("push!!!!");
+        replier.reply("push!!!!!!");
     }   
 }
