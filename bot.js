153a138dfdc361af831f563568a365b23e2c91cb
Tree Some <youn@tree-some.dev>
2020-06-05 02:07:34 +0900

    bot 수정

--- bot.js
+++ bot.js
@@ -1,17 +1,23 @@
 Jsoup = org.jsoup.Jsoup;
 
 const recvHook = () => {
-	while ( true ) {
+	try {
 		const res = Jsoup.connect("https://us-central1-kakaobot-63224.cloudfunctions.net/hook/recv-build-hook")
 			.timeout(1000 * 60 * 60)
 			.get();
 
-		if ( res.statusCode() === 200 ) {
-			code = res.body().text();
-			Log.d(code);
-			eval(code);
+		json = JSON.parse(res.body().text());
+
+		if ( json.st === 200 ) {
+			Log.d("수정된 코드를 적용합니다.");
+			eval(json.code);
 		}
+	} catch(err) {
+		Log.d(err);
 	}
+
+	setTimeout(recvHook, 1000 * 60);
 };
 
+Log.d("Start recvHook Polling", true);
 setTimeout(recvHook, 0);
