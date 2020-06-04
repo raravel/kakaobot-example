e7bf5337e88ad436f8810d5e9b9e75c9687d9c65
Tree Some <youn@tree-some.dev>
2020-06-05 00:22:33 +0900

    hook 추가

---
+++ hook.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+const fs = require('fs');
+
+if ( fs.existsSync('dist/index.js') ) {
+	fs.readFile('dist/index.js', { encoding: 'utf8' }, (err, data) => {
+		axios({
+			url: 'https://us-central1-kakaobot-63224.cloudfunctions.net/hook/build-complete',
+			method: 'put',
+			data: {
+				code: data,
+			},
+		});
+	});
+}
+
